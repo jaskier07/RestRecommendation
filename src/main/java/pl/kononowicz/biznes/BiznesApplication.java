4b64eece1361dfa90e64c4731353954fa7e989db
jaskier07 <aleksanderkania96@gmail.com>
2018-01-18 13:02:18 +0100

    Pierwszy commit

---
+++ src/main/java/pl/kononowicz/biznes/BiznesApplication.java
@@ -0,0 +1,22 @@
+package pl.kononowicz.biznes;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiznesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiznesApplication.class, args);
+	}
+}
+
+/*
+
+
+
+(scanBasePackages={
+"pl.kononowicz.biznes.MainController", "pl.kononowicz.biznes.Author"})
+*/
